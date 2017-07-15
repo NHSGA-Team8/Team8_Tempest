b230f304adaeedf7bc527b609fb918b3ad006ff7
LAPTOP-M7VGAAAU\Adebi Family <iadebi@andrew.cmu.edu>
2017-07-15 16:21:33 -0400

    no message

---
+++ Tempest/Assets/Scripts/IShipBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// An interface for all ships, friendly and enemy.
+	public interface IShipBase {
+
+	// Called to fire a projectile.
+	void Fire();
+
+	// Called when a projectile damages the ship. Should call OnDeath() if it kills;
+	void TakeDamage(int dmg);
+
+	// Called when the ship dies. Add points, do game state detection, etc.
+	void OnDeath();
+}
