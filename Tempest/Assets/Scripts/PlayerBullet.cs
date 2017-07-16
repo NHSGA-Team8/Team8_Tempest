0e2106eedb07a03344fb3dc190df7fceeabc3984
TheOnly8Z <888888zombies@gmail.com>
2017-07-15 18:53:18 -0900

    Created BaseBullet and PlayerBullet

---
+++ Tempest/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : BaseBullet {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
