105437e0c72e3029286138e8a9414712cc166530
Rachael H <rachael.h1741@gmail.com>
2017-07-16 01:12:33 -0400

    Flipper Scale and Class

---
+++ Tempest/Assets/Scripts/Flipper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : IShipBase {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Called to fire a projectile.
+	public void Fire()
+	{
+	}
+
+	// Called when a projectile damages the ship. Should call OnDeath() if it kills;
+	public void TakeDamage(int dmg)
+	{
+	}
+
+	// Called when the ship dies. Add points, do game state detection, etc.
+	public void OnDeath()
+	{
+	}
+}
