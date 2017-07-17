242cde4bfb9cfb2cb5c8559475ccbf9979711067
LAPTOP-M7VGAAAU\Adebi Family <iadebi@andrew.cmu.edu>
2017-07-17 16:51:50 -0400

    no message

--- Tempest/Assets/Scripts/GameManager.cs
+++ Tempest/Assets/Scripts/GameManager.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	public GameObject flipperPrefab;
+	public int totalFlippers;
+	public float roundTotalTime;
+	public int currentRound;
+	public int nextScene;
+
+	private float _lastSpawn;
+	private int _flipperCount;
+	private Flipper[] flippers;
+
 	// Use this for initialization
 	void Start () {
-		
+		flippers = new Flipper[totalFlippers];
 	}
 	
 	// Update is called once per frame
